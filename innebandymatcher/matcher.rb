c7a0a6f716604dc97f8b8ae0d17fe368863bec0a
bjorn <bjorn@ubuntu.waller.se>
2014-07-20 16:07:01 +0200

    Uses DateTime class to handle matchtime. Also some variables to control event start and stop, season is taken as input etc.

--- innebandymatcher/matcher.rb
+++ innebandymatcher/matcher.rb
@@ -3,8 +3,26 @@
 require 'open-uri'
 require 'rubygems'
 require 'nokogiri'
+require 'date'
 
-uri = "http://www.stockholminnebandy.nu/FA/Lagmatcher.asp?LAGID="+ARGV[0]+"&SFKOD=58&SDFKOD=15&SASONG=2013"
+veckodag = Array.new(7)
+veckodag[1] = "mandag"
+veckodag[2] = "tisdag"
+veckodag[3] = "onsdag"
+veckodag[4] = "torsdag"
+veckodag[5] = "fredag"
+veckodag[6] = "lordag"
+veckodag[7] = "sondag"
+
+lagid = ARGV[0]
+sfkod = 58.to_s
+sdfkod = 15.to_s
+season = ARGV[1]
+
+event_starts_minutes_before_match = 45
+event_ends_minutes_after_match = 60
+
+uri = "http://www.stockholminnebandy.nu/FA/Lagmatcher.asp?LAGID="+lagid+"&SFKOD="+sfkod+"&SDFKOD="+sfkod+"&SASONG="+season
 doc = Nokogiri::HTML(open(uri))
 doc.css('tr').each do |element|
   row = element.text.split("\r\n")
@@ -13,31 +31,32 @@ doc.css('tr').each do |element|
     month = row[1].partition("/")[2].to_i
     minute = row[2].partition(":")[2].to_i
     hour = row[2].partition(":")[0].to_i
-    if month > 6
-      year = Time.now.year
-    else 
-      year = Time.now.year + 1
+    year = season.to_i
+    if month < 6
+      year = season.to_i + 1
     end
-    matchtime = Time.local(year,month,day,hour,minute,0) 
-    eventstart = (matchtime-(60*45)).strftime("%H:%M")
-    eventslut = (matchtime+(60*60)).strftime("%H:%M")
-    startdatum = matchtime.strftime("%Y-%m-%d")
-    stopdatum = startdatum
+    matchstart = DateTime.new(year,month,day,hour,minute)
+    eventstart = matchstart + Rational(-event_starts_minutes_before_match,60*24)
+    eventend = matchstart + Rational(event_ends_minutes_after_match,60*24)
+    answerdate = matchstart - 3
 
     hemmalag = row[4].strip
     bortalag = row[5].strip
 
     arena = row[7].strip
+    if arena.match("^Tappstr.msskolan")
+      arena = "Bollhallen"
+    end
 
     print "Matcher;"
     print hemmalag, " vs ", bortalag,";"
     print arena,";" 
-    print "Matchstart ", matchtime.strftime("%H:%M"),". Osa senast onsdag 22.00.",";"
-    print eventstart,";"
-    print eventslut,";"
-    print startdatum,";"
-    print stopdatum,";"
-    print "Bjorn Waller"
+    print "Matchstart ", matchstart.strftime("%H:%M"),". Osa senast ", veckodag[answerdate.cwday], " 22.00.",";"
+    print eventstart.strftime("%H:%M"),";"
+    print eventend.strftime("%H:%M"),";"
+    print matchstart.strftime("%Y-%m-%d"),";"
+    print matchstart.strftime("%Y-%m-%d"),";"
+    print 'Bjorn Waller'
     print "\n"
   end
 end
