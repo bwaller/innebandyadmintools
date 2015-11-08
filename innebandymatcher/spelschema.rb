257f72a5c64a3e0a3a9d5a2fda90ae1d7cd88552
Björn Waller <bjorn@waller.nu>
2015-11-08 10:02:58 +0100

    Work in progress

--- innebandymatcher/spelschema.rb
+++ innebandymatcher/spelschema.rb
@@ -8,14 +8,16 @@ filename = ARGV[0]
 
 class Event
 
-  attr_accessor :arena, :series
+  attr_accessor :arena, :series, :start_time, :end_time, :opponents
 
-  def initialize(series, start_date_str, start_clock_str, length, arena)
+  def initialize(series, start_date_str, start_clock_str, length, opponents, arena)
     @series = String.new(series.to_s)
     @start_time = DateTime.iso8601(start_date_str+"T"+start_clock_str+":00+01:00")
     @length = length #In minutes
     @end_time = @start_time + Rational(length,24*60)
+    @opponents = String.new(opponents.to_s)
     @arena = String.new(arena.to_s)
+    @@home_arena = "Tappströms Bollhall"
   end
 
   def eventdate_str
@@ -28,6 +30,18 @@ class Event
 
   def end_time_str
     return @end_time.strftime("%T")
+  end
+
+  def text
+    return @arena == @@home_arena ? "Hemma" : "Borta"
+  end
+
+  def home
+    return @arena == @@home_arena ? true : false
+  end
+
+  def away
+    return @arena == @@home_arena ? false : true
   end
 
 end # game
@@ -67,6 +81,12 @@ class EventWorksheet < Writeexcel::Worksheet
     @starthour = 8
     @endhour = 21
     @time_format = workbook.add_format(:num_format => 'hh:mm')
+    @home_top_format = workbook.add_format(:top => 1, :left => 1, :right => 1, :bg_color => 'blue')
+    @home_side_format = workbook.add_format(:left => 1, :right => 1, :bg_color => 'blue')
+    @home_bottom_format = workbook.add_format(:bottom => 1, :left => 1, :right => 1, :bg_color => 'blue')
+    @away_top_format = workbook.add_format(:top => 1, :left => 1, :right => 1, :bg_color => 'yellow')
+    @away_side_format = workbook.add_format(:left => 1, :right => 1, :bg_color => 'yellow')
+    @away_bottom_format = workbook.add_format(:bottom => 1, :left => 1, :right => 1, :bg_color => 'yellow')
     print_rows
   end 
 
@@ -82,15 +102,28 @@ class EventWorksheet < Writeexcel::Worksheet
     end
   end
 
+  def getformat (event)
+    if (event.home) then
+      return @home_top_format, @home_side_format, @home_bottom_format
+    else
+      return @away_top_format, @away_side_format, @away_bottom_format
+    end
+  end
+
   def print_column (year, week, col, events, head_format = @head_format)
     (Date.commercial(year,week,5)..Date.commercial(year, week, 7)).each do |date|
       self.write(0, col, date.strftime(head_format))
       if event = events[date.strftime("%F")] then
-        puts event.eventdate_str + " " + event.start_time_str + " " + event.end_time_str + " " + event.arena
+        row = 1 + (event.start_time.hour-@starthour)*4+event.start_time.minute/15
+        top_format, side_format, bottom_format = self.getformat(event)
+        self.write(row, col, event.series, top_format)
+        self.write(row + 1, col, event.opponents, side_format)
+        self.write(row + 2, col, event.arena, side_format)
+        self.write_blank(row + 3, col, bottom_format)
       end
-      col 
+      col += 1 
     end
-    return col
+    return col 
   end
 
   def print_columns(events)
@@ -107,16 +140,20 @@ class EventWorksheet < Writeexcel::Worksheet
 end
 
 #test
-myfirstevent = Event.new("P-LR-M-D","2015-11-15","14:00", 60, "Tappströms Bollhall")
-mysecondevent = Event.new("P-LR-M-D","2016-01-12","16:00", 60, "Tappströms Bollhall")
-#puts myfirstevent.eventdate_str + " " + myfirstevent.start_time_str + " " + myfirstevent.end_time_str + " " + myfirstevent.arena
-
 events = Hash.new
-events[myfirstevent.eventdate_str] = myfirstevent
-events[mysecondevent.eventdate_str] = mysecondevent
+event = Event.new("P-LR-M-D","2015-11-15","14:00", 60, "Hässelby IK", "Tappströms Bollhall")
+events[event.eventdate_str] = event
+event = Event.new("P-LR-M-D","2015-11-20","14:15", 60, "Kungsängen IF", "IF Hallen")
+events[event.eventdate_str] = event
+event = Event.new("P-LR-M-D","2015-11-21","14:30", 60, "Ingarö IF (B)", "Tappströms Bollhall")
+events[event.eventdate_str] = event
+event = Event.new("P-LR-M-D","2015-11-22","14:45", 60, "Sundbybergs IK (A)", "Eriksdalshallen")
+events[event.eventdate_str] = event
+event = Event.new("P-LR-M-D","2016-01-12","16:00", 60, "Huddinge IBF", "Tappströms Bollhall")
+events[event.eventdate_str] = event
 
 workbook = EventWorkbook.new(filename) 
-worksheet = workbook.add_worksheet(DateTime.now.year, 35, 15)
+worksheet = workbook.add_worksheet(DateTime.now.year, 45, 15)
 worksheet.print_columns(events)
 
 workbook.close
