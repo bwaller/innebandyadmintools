6fb8c1863d260b0f974b4080f636baf62d02e73e
Björn Waller <bjorn@waller.nu>
2015-11-09 20:14:10 +0100

    width -> 8, separate function for drawing hours

--- innebandymatcher/schedule.rb
+++ innebandymatcher/schedule.rb
@@ -18,11 +18,11 @@ class GameSchedule
     @hour_points = (@y_axis_top/(enddate.hour-startdate.hour)).to_i
     @minute_points = (@hour_points/60).to_i
     @quarter_points = (@hour_points/4).to_i
-    @column_width = 12
+    @column_width = 8
     @time_points = Hash.new
   end
 
-  def draw_vertical_line(xpos, render_time = false)
+  def draw_vertical_line(xpos, store_time_points = false)
     minor_width = 2
     major_width = 4
     width = major_width
@@ -30,20 +30,27 @@ class GameSchedule
     
     quarters=0
     @y_axis_top.step(0, -@quarter_points) do |ypos|
-      time_str = (@startdate + Rational(quarters,24*4)).strftime("%H:%M")
+      if (store_time_points) then 
+        time_str = (@startdate + Rational(quarters,24*4)).strftime("%H:%M")
+        @time_points[time_str] = ypos
+      end
       if (quarters % 4 == 0) then 
         width = major_width 
-        if (render_time) 
-          draw_text time_str, :at => [xpos-24, ypos-3], :size => 8
-        end
       else
         width = minor_width
       end
       horizontal_line xpos-width, xpos+width, :at => ypos
-      @time_points[time_str] = ypos
       quarters += 1
     end
     stroke
+  end
+
+  def draw_hours (xpos)
+    @time_points.each do |time_str, ypos|
+      if (time_str.match(/:00$/)) then
+        draw_text time_str, :at => [xpos-24, ypos-3], :size => 8
+      end
+    end
   end
 
   def draw_event_box( col, event_date, color_str = "FFFFFF")
@@ -64,12 +71,14 @@ schedule = GameSchedule.new("2015-2016",
                             DateTime.new(startyear, 10, 1, 9, 0, 0, 1),
                             DateTime.new(endyear, 4, 15, 21, 0, 0, 1))
 (0..schedule.column_width*3*3*7).step(schedule.column_width*9) do |x|
-  if x == 0
+  if x == 0 then
     schedule.draw_vertical_line x, true
-  else 
+  else
     schedule.draw_vertical_line x
   end
 end
+schedule.draw_hours(50)
+
 schedule.draw_event_box(0, DateTime.new(startyear, 10, 1, 9, 0, 0, 1), "FFFF00")
 schedule.draw_event_box(1, DateTime.new(startyear, 10, 1, 10, 0, 0, 1))
 schedule.draw_event_box(2, DateTime.new(startyear, 10, 1, 11, 0, 0, 1), "0000FF")
@@ -81,5 +90,6 @@ schedule.draw_event_box(7, DateTime.new(startyear, 10, 1, 15, 15, 0, 1))
 schedule.draw_event_box(8, DateTime.new(startyear, 10, 1, 15, 30, 0, 1))
 schedule.draw_event_box(9, DateTime.new(startyear, 10, 1, 15, 45, 0, 1))
 schedule.draw_event_box(10, DateTime.new(startyear, 10, 1, 16, 0, 0, 1))
+
 schedule.save_as("schedule.pdf")
 
