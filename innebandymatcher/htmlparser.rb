eb27b85d9c04fd623d4381dc63b8ec3936811713
Björn Waller <bjorn@waller.nu>
2015-11-15 23:02:34 +0100

    Parses relevant event info

--- innebandymatcher/htmlparser.rb
+++ innebandymatcher/htmlparser.rb
@@ -7,10 +7,22 @@ require 'open-uri'
 require './event.rb'
 
 page = Nokogiri::HTML(open("http://statistik.innebandy.se/ft.aspx?scr=teamresult&flid=494"))   
-#games = page.css("html body div#container div#IbisInfo.ibisinfo table.clCommonGrid tbody.clGrid").text 
-games = page.css("html body div#container div#IbisInfo.ibisinfo table.clCommonGrid tbody.clGrid tr")
-games.each do |game|
-  puts game
+events = page.css("html body div#container div#IbisInfo.ibisinfo table.clCommonGrid tbody.clGrid tr")
+events.each do |event|
+  event.css("span").each do |span|
+    puts "New game: "
+    event_date = DateTime.parse(span.content)
+    home_team, away_team, arena = ""
+    event.css("a").each do |element|
+      if element["href"].match("result") and !element["class"]  then
+        home_team, away_team = element.content.split(" - ")
+        home_team.strip
+        away_team.strip
+      end
+      arena = element.content if element["href"].match("venue")
+    end
+    puts event_date.strftime("%F %T") + " " + home_team + " vs " + away_team + " at " + arena
+  end  
 end
 
 
