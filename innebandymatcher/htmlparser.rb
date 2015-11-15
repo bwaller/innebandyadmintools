3c0fe68080f01c034bb6e42dd7f4a13e7b9f8204
Björn Waller <bjorn@waller.nu>
2015-11-15 21:11:27 +0100

    Finds all 'games'

--- innebandymatcher/htmlparser.rb
+++ innebandymatcher/htmlparser.rb
@@ -8,15 +8,9 @@ require './event.rb'
 
 page = Nokogiri::HTML(open("http://statistik.innebandy.se/ft.aspx?scr=teamresult&flid=494"))   
 #games = page.css("html body div#container div#IbisInfo.ibisinfo table.clCommonGrid tbody.clGrid").text 
-games = page.css("html body div#container div#IbisInfo.ibisinfo table.clCommonGrid tbody.clGrid td")
-games.each do |node|
-  node.elements.each do |element|
-    if element['href'] then
-      if element['href'].match('/venue') then
-        puts element
-      end
-    end
-  end
+games = page.css("html body div#container div#IbisInfo.ibisinfo table.clCommonGrid tbody.clGrid tr")
+games.each do |game|
+  puts game
 end
 
 
