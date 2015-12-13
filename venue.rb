9d0c09f57f28ac33e72ccb607e3a31e2a404eebc
Björn Waller <bjorn@waller.nu>
2015-12-13 07:55:50 +0100

    Replaced get(id) with initialize(id).

--- venue.rb
+++ venue.rb
@@ -9,70 +9,60 @@ require 'json'
 
 class Venue
 
-  def initialize(id, name, streetaddress, postal_code, locality)
-    @id = id.to_i
-    @name = name
-    @streetaddress = streetaddress
-    @postal_code = postal_code
-    @locality = locality
-  end
-  
-  def initialize(id, json)
-    @id = id
-    array = JSON.parse(json)
-    @name = array[0]
-    @streetaddress = array[1]
-    @postal_code = array[2]
-    @locality = array[3]
-  end
-
   attr_accessor :id, :name, :streetaddress, :postal_code, :locality
 
-  def to_s
-    return @name + " " + @streetaddress + " " + @postal_code + " " + @locality 
-  end
- 
-  def to_ruby
-    puts '"' + @name + '" => Venue.new("' + @name + '", "' + @streetaddress + '", "' + @postal_code + '", "' + @locality + '"),'
-  end
+  def initialize (id)
 
-  def faid
-    return id
-  end
-
-  def self.getvenue(id)
-
-    if venue_as_json = @@redis.get(id) then
-      return Venue.new(id, venue_as_json)
+    @id = id
+    key = Venue.key(@id)
+    if json_str = @@redis.get(key) then
+      array = JSON.parse(json_str)
+      @name = array[0]
+      @streetaddress = array[1]
+      @postal_code = array[2]
+      @locality = array[3]
     else
-      name, streetaddress, postal_code, locality = ""
       venue_url = "http://statistik.innebandy.se/ft.aspx?scr=venue&faid=" + id.to_s
       venue_doc = Nokogiri::HTML(open(venue_url))
       if !element = venue_doc.at_css("html body div#container div#IbisInfo.ibisinfo div.clFogis h1") then
         puts "Url " + venue_url + " returned empty document"
         return nil
       end
-      name = element.content
+      @name = element.content
       if element = venue_doc.at_css("html body div#container div#IbisInfo.ibisinfo div.clFogis dl dd span.street-address") then 
-        streetaddress = element.content
+        @streetaddress = element.content
       else 
-        streetaddress = "STREET_UNDEFINED"
+        @streetaddress = "STREET_UNDEFINED"
       end
       if element = venue_doc.at_css("html body div#container div#IbisInfo.ibisinfo div.clFogis dl dd span.postal-code") then
-        postal_code = element.content
+        @postal_code = element.content
       else 
-        postal_code = "POSTAL_CODE_UNDEFINED"
+        @postal_code = "POSTAL_CODE_UNDEFINED"
       end
       if element = venue_doc.at_css("html body div#container div#IbisInfo.ibisinfo div.clFogis dl dd span.locality") then
-        locality = element.content 
+        @locality = element.content 
       else 
-        locality = "LOCALITY_UNDEFINED"
+        @locality = "LOCALITY_UNDEFINED"
       end
-      json_str = [name, streetaddress, postal_code, locality].to_json
-      @@redis.set id, json_str
-      return Venue.new(id, json_str)
+      Venue.set(@id, @name, @streetaddress, @postal_code, @locality)
     end 
+  end
+  
+  def self.set(id, name, streetaddress, postal_code, locality)
+    json_str = [name, streetaddress, postal_code, locality].to_json
+    @@redis.set self.key(id), json_str
+  end
 
+  def self.key(id)
+    return key = self.class.to_s+id.to_s
+  end
+
+  def to_s
+    return @name + " " + @streetaddress + " " + @postal_code + " " + @locality 
+  end
+ 
+  def faid
+    return id
   end
 
 end
