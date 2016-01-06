e7067b2bd419f9d54e7c798f3fe1848e8540ef4b
Björn Waller <bjorn@waller.nu>
2016-01-06 07:32:23 +0100

    Added route for '/'

--- sinatra.rb
+++ sinatra.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 #set :port, 80
  
+get '/' do
+ "Generate excelfiles for Sportnik import"
+end
+
 get '/generate' do
   team_id=params['team_id'].gsub("+"," ")
   command = "/usr/bin/ruby parse.rb " + team_id
