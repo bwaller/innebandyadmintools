c97c44db829477181c164197659391bc185fa918
Björn Waller <bjorn@waller.nu>
2016-01-06 12:11:36 +0100

    Debugging...

--- sinatra.rb
+++ sinatra.rb
@@ -5,14 +5,14 @@ set :bind, '0.0.0.0'
  
 get '/' do
   date=`date`
- "Generate excelfiles for Sportnik import" + date.to_s
+ "Generate excelfiles for Sportnik import<br>" + date.to_s
 end
 
 get '/generate' do
   team_id=params['team_id'].gsub("+"," ")
-#  command = "/usr/bin/ruby parse.rb " + team_id
-#  result_file = `#{command}`.split(" ").last
-#  send_file result_file, :filename => result_file, :type => 'Application/octet-stream' 
+  command = "ruby parse.rb " + team_id
+  result_file = `#{command}`.split(" ").last
+  send_file result_file, :filename => result_file, :type => 'Application/octet-stream' 
  "team_id: is #{team_id}"
 end
 
