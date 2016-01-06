b770d8745a278105338f2765b00de7d0f1090678
Björn Waller <bjorn@waller.nu>
2016-01-06 12:07:19 +0100

    Debugging...

--- sinatra.rb
+++ sinatra.rb
@@ -9,8 +9,9 @@ end
 
 get '/generate' do
   team_id=params['team_id'].gsub("+"," ")
-  command = "/usr/bin/ruby parse.rb " + team_id
-  result_file = `#{command}`.split(" ").last
-  send_file result_file, :filename => result_file, :type => 'Application/octet-stream' 
+#  command = "/usr/bin/ruby parse.rb " + team_id
+#  result_file = `#{command}`.split(" ").last
+#  send_file result_file, :filename => result_file, :type => 'Application/octet-stream' 
+  echo "team_id: is #{team_id}"
 end
 
