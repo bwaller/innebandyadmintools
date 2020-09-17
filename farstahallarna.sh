1f944c98e2402f908bfcfb800fbc46cfa9fc20ed
Björn W <bjorn@waller.nu>
2020-09-17 19:07:35 +0000

    Add parameter for ics generation
    Also change output file for crop command

--- farstahallarna.sh
+++ farstahallarna.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ruby gen_ics_venue.rb 1256  > farstahallen1.ics
-ruby gen_ics_venue.rb 1258  > farstahallen3.ics
+ruby gen_ics_venue.rb -i 1256  > farstahallen1.ics
+ruby gen_ics_venue.rb -i 1258  > farstahallen3.ics
 
 ruby rmagick.rb farstahallen1.ics farstahallen3.ics
-convert junk.png -crop 840x1188 -page 840x1188 /mnt/hgfs/Documents/farstahallarna.pdf
+convert junk.png -crop 840x1188 -page 840x1188 /vagrant/farstahallarna.pdf
